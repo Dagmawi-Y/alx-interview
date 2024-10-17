6b4028706dca9b32ababbf91f89e4b4cf5a38a09
Dagmawi-Y <dagmawi.inbox@gmail.com>
2024-10-18 01:45:44 +0300

    fixes

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,16 +7,29 @@ def minOperations(n):
     '''Computes the fewest number of operations needed to result
     in exactly n H characters.
     '''
-    if n <= 1:
+    if not isinstance(n, int):
         return 0
-    
     ops_count = 0
-    divisor = 2
-    
-    while n > 1:
-        while n % divisor == 0:
-            ops_count += divisor
-            n //= divisor
-        divisor += 1
-    
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
     return ops_count
