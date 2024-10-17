ba9f4c5b6bb5894d4bdd435b5b7017c3d8448b2f
Dagmawi-Y <dagmawi.inbox@gmail.com>
2024-10-18 01:54:24 +0300

    iter. 6

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,35 +1,21 @@
 #!/usr/bin/python3
-"""
-Module that contains the minOperations function.
-"""
-
-
-def minOperations(n):
+""" Minimum Operations
     """
-    Calculates the fewest number of operations needed to result in exactly n H
-    characters in a file.
 
-    Args:
-        n (int): The target number of H characters.
 
-    Returns:
-        int: The minimum number of operations needed to achieve n H characters.
-             Returns 0 if n is impossible to achieve or if n is not a positive int.
-    """
-    if not isinstance(n, int) or n <= 1:
+def minOperations(n: int) -> int:
+    """ Minimum Operations needed to get n H characters """
+    next = 'H'
+    body = 'H'
+    op = 0
+    while (len(body) < n):
+        if n % len(body) == 0:
+            op += 2
+            next = body
+            body += body
+        else:
+            op += 1
+            body += next
+    if len(body) != n:
         return 0
-
-    operations = 0
-    divisor = 2
-
-    while n > 1:
-        while n % divisor == 0:
-            operations += divisor
-            n = n // divisor
-        divisor += 1
-
-        if divisor * divisor > n and n > 1:
-            operations += n
-            break
-
-    return operations
+    return op
